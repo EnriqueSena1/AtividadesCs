33c91a66a181c764dd5ded30eb052d8e98f10a45
EnriqueSena1 <enriquesenacontato07@gmail.com>
2025-04-19 18:29:05 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadesCs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExceciciosC_ {
+
+    class teste {
+        static void Main(string[] args) {
+            int idade;
+            double salario;
+            string nome;
+            char sexo;
+
+            idade = 32;
+            salario = 4560.9;
+            nome = "Maria Silva";
+            sexo = 'F';
+
+            Console.WriteLine("A funcionaria" + nome);
+
+
+
+        }
+    }
+}
