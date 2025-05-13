99820a36c70cdcb99bd970bb0c200ad8e010249a
EnriqueSena1 <enriquesenacontato07@gmail.com>
2025-05-13 06:36:25 -0300

    Feature-Teste de Horas

---
+++ Atividade Verificando Dia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace EntradaDados {
+    class Program {
+        static void Main(string[] args) {
+            //CI = CultureInfo.InvariantCulture;
+            int hora;
+            Console.Write("Digite uma Hora do dia: ");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) {
+                Console.WriteLine("Bom dia!!!");
+            }
+            else {
+                Console.WriteLine("Boa tarde !!!");
+            }
+
+        }
+    }
+}
